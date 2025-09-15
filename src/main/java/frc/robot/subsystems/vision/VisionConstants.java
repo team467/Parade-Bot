bc53f9c6976a6bd8def2ab9cced7752f952b31ab
Bhoomi Prashanth <bhoomiprashanth10@gmail.com>
2025-09-15 19:52:27 -0400

    base of vision constants

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -1,5 +1,50 @@
 package frc.robot.subsystems.vision;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.util.Units;
+
 public class VisionConstants {
-    
+  // AprilTag layout
+  public static AprilTagFieldLayout aprilTagLayout =
+      AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+
+  // Camera names, must match names configured on coprocessor
+  public static String camera0Name = "camera_0";
+  public static String camera1Name = "camera_1";
+
+  // Robot to camera transforms
+  // (Not used by Limelight, configure in web UI instead)
+  public static Transform3d robotToCamera0 =
+      new Transform3d(
+          Units.inchesToMeters(13),
+          Units.inchesToMeters(0),
+          Units.inchesToMeters(6.5),
+          new Rotation3d(0.0, Units.degreesToRadians(-37.5), 0));
+  public static Transform3d robotToCamera1 =
+      new Transform3d(
+          Units.inchesToMeters(-14),
+          Units.inchesToMeters(0),
+          Units.inchesToMeters(6.25),
+          new Rotation3d(0.0, Units.degreesToRadians(-68), Math.PI));
+
+  // Basic filtering thresholds
+  public static double maxAmbiguity = 0.3;
+  public static double maxZError = 0.75;
+  // PhotonEstimator strategy
+
+  // Standard deviation baselines, for 1 meter distance and 1 tag
+  // (Adjusted automatically based on distance and # of tags)
+  public static double linearStdDevBaseline = 0.15; // Meters
+  public static double angularStdDevBaseline = 0.25; // Radians
+
+  // Standard deviation multipliers for each camera
+  // (Adjust to trust some cameras more than others)
+  public static double[] cameraStdDevFactors =
+      new double[] {
+        1.0, // Camera 0
+        1.0 // Camera 1
+      };
 }
