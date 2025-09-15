3e2da8cec0f339549e46d629003a0b592b5ddbe5
TheKitchenPan1 <153659491+TheKitchenPan1@users.noreply.github.com>
2025-09-15 19:51:00 -0400

    ModuleIOInputs

--- src/main/java/frc/robot/subsystems/drive/ModuleIO.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIO.java
@@ -9,6 +9,40 @@ public interface ModuleIO{
         public double volts = 0.0;
         public double current = 0.0;
 
+        public boolean driveConnected = false;
+        public double drivePositionRad = 0.0;
+        public double driveVelocityRadPerSec = 0.0;
+        public double driveAppliedVolts = 0.0;
+        public double driveCurrentAmps = 0.0;
+
+        public boolean turnConnected = false;
+        public Rotation2d turnPosition = Rotation2d.kZero;
+        public double turnVelocityRadPerSec = 0.0;
+        public double turnAppliedVolts = 0.0;
+        public double turnCurrentAmps = 0.0;
+
+        public double turnPositionAbsolute = 0.0;
+
+        public double[] odometryTimestamps = new double[] {};
+        public double[] odometryDrivePositionsRad = new double[] {};
+        public Rotation2d[] odometryTurnPositions = new Rotation2d[] {};
     }
+
+    /** Updates the set of loggable inputs. */
+    public default void updateInputs(ModuleIOInputs inputs) {}
+
+    /** Run the drive motor at the specified open loop value. */
+    public default void setDriveOpenLoop(double output) {}
+
+    /** Run the turn motor at the specified open loop value. */
+    public default void setTurnOpenLoop(double output) {}
+
+    /** Run the drive motor at the specified velocity. */
+    public default void setDriveVelocity(double velocityRadPerSec) {}
+
+    /** Run the turn motor to the specified rotation. */
+    public default void setTurnPosition(Rotation2d rotation) {}
+
 }
+
 
