b48a9a1f010479e9ab0a89bac4133889f25196dc
pranavgundu <pranav@gundu.me>
2025-09-15 20:25:40 -0400

    outline

---
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.function.DoubleSupplier;
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
+
+public class Shooter extends SubsystemBase {
+
+}
+
---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.shooter;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ShooterIO {
+    @AutoLog
+    class CoralEffectorIOInputs {
+    }
+}
