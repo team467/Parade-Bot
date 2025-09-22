6a4397b18b490b12f1f925d9ec22ffc2e67de9f5
whoSohum <ssriv27@students.shrewsbury.k12.ma.us>
2025-09-22 19:44:10 -0400

    Update Indexer.java
    Import packages for ShooterIOPhysical

--- src/main/java/frc/robot/subsystems/shooter/ShooterIOPhysical.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOPhysical.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems.shooter;
-
+import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revreobotics.RelativeEncoder;
 public class ShooterIOPhysical implements ShooterIO {
 
 }
