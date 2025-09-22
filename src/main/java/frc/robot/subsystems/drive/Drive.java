d61967cd710423961f9bd7acc8ad77250ae4c896
EQMoore <117695748+EQMoore@users.noreply.github.com>
2025-09-22 12:07:17 -0400

    Initialize drive voltageConsumers in Drive class

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -4,10 +4,11 @@ package frc.robot.subsystems.drive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drive extends SubsystemBase {
-
+DoubleConsumer setLeftDriveVoltage;
+DoubleConsumer setRightDriveVoltage 
 
     public Drive(DriveIO io){
-
-
+        setLeftDriveVoltage = (voltage) -> DriveIO.setVoltageLeft(voltage);
+        setRightDriveVoltage = (voltage) -> DriveIO.setVoltageRight(voltage);
     }
 }
