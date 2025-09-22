bbc767cbf070d940a950f917c5cf1f5a53795028
EQMoore <117695748+EQMoore@users.noreply.github.com>
2025-09-22 12:45:09 -0400

    Integrate DriveIO and add periodic logging

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -6,14 +6,23 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
+import org.littletonrobotics.junction.Logger;
 
 public class Drive extends SubsystemBase {
-private final DifferentialDrive differentialDrive;
+    private final DriveIO io;
+    private final DifferentialDrive differentialDrive;
     public Drive(DriveIO io){
+        this.io = io;
         differentialDrive = new DifferentialDrive(io::setVoltageLeft, io::setVoltageRight);
     }
     public Command curvatureDrive(DoubleSupplier speedX, DoubleSupplier rotationZ, BooleanSupplier turnInPlace) {
-     return differentialDrive.curvatureDrive(speedX.getAsDouble(), rotationZ.getAsDouble(), turnInPlace.getAsBoolean());
+        return Commands.run(
+            () -> differentialDrive.curvatureDrive(speedX.getAsDouble(), rotationZ.getAsDouble(), turnInPlace.getAsBoolean());
+        );
     }
-
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Drive", inputs);
+    }
 }
