0dbb1a9d45111c6575055d6f47039774fb91f01c
rkris28 <rkris28@students.shrewsbury.k12.ma.us>
2025-09-24 19:52:28 -0400

    Tunes Shooter_Rotations
    Tunes Shooter_Rotations

--- src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
@@ -12,5 +12,5 @@ public class IndexerConstants {
 
     public static final double ENCODER_VELOCITY_CONVERSION = 1.0;
 
-    public static final double SHOOTER_ROTATIONS = 0.0;
+    public static final double SHOOTER_ROTATIONS = 0.5;
 }
