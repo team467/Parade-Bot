330e76f33b5c7f4cb740101ec03b9151b2277274
Niti <ntyag28@students.shrewsbury.k12.ma.us>
2025-10-01 19:31:58 -0400

    set up inputs

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -1,5 +1,28 @@
 package frc.robot.subsystems.vision;
 
-public class Vision {
-    
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Vision extends SubsystemBase {
+    private final VisionConsumer consumer;
+    private final VisionIO[] io;
+    private final VisionIOInputsAutoLogged[] inputs;
+    private final Alert[] disconnectedAlerts;
+
+    public Vision(VisionConsumer consumer, VisionIO... io) {
+        this.consumer = consumer;
+        this.io = io;
+
+        this.inputs = new VisionIOInputsAutoLogged[io.length];
+        for (int i = 0; i < inputs.length; i++) {
+            inputs[i] = new VisionIOInputsAutoLogged();
+        }
+
+        this.disconnectedAlerts = new Alert[io.length];
+        for (int i = 0; i < inputs.length; i++) {
+            disconnectedAlerts[i] =
+                    new Alert(
+                            "Vision camera " + Integer.toString(i) + " is disconnected.", Alert.AlertType.kWarning);
+        }
+    }
 }
