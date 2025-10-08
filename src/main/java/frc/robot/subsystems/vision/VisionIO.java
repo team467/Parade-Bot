d2bea0a75af7bf4b2e68d59a9b026d41aafbae41
Niti <ntyag28@students.shrewsbury.k12.ma.us>
2025-10-07 21:15:13 -0400

    set up inputs

--- src/main/java/frc/robot/subsystems/vision/VisionIO.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIO.java
@@ -1,35 +1,26 @@
 package frc.robot.subsystems.vision;
 
-import org.littletonrobotics.junction.AutoLog;
-
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
+import org.littletonrobotics.junction.AutoLog
 
 public interface VisionIO {
   @AutoLog
   public static class VisionIOInputs {
     public boolean connected = false;
-    public TargetObservation latestTargetObservation =
-        new TargetObservation(new Rotation2d(), new Rotation2d());
-    public PoseObservation[] poseObservations = new PoseObservation[0];
-    public int[] tagIds = new int[0];
+    public int distanceFromTarget = 0;
+    public double yaw = 0.0;
+    public int tagId = 0;
   }
-
-  /** Represents the angle to a simple target, not used for pose estimation. */
-  public static record TargetObservation(Rotation2d tx, Rotation2d ty) {}
-
-  /** Represents a robot pose sample used for pose estimation. */
-  public static record PoseObservation(
-      double timestamp,
-      Pose3d pose,
-      double ambiguity,
-      int tagCount,
-      double averageTagDistance,
-      PoseObservationType type) {}
 
   public static enum PoseObservationType {
     PHOTONVISION
   }
 
+  default void getDistanceFromTarget(double targetX, double targetY) {}
+  default void getYaw(double yaw) {}
+
   public default void updateInputs(VisionIOInputs inputs) {}
 }
+// yaw
+//distance from target
+// tags
+//connected
