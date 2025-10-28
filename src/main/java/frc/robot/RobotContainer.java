128b5dfd0d3b4c19bf03b5bb5f5731e656dd26b4
ShayanHásHackedThis <shayansteel117@gmail.com>
2025-10-27 20:51:22 -0400

    switch to ff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -79,7 +79,7 @@ public class RobotContainer {
                       .onFalse(orchestrator.stopAll());
 
               //driverController.x().whileTrue(orchestrator.shootCycleDistance()).onFalse(shooter.stop());
-              driverController.x().whileTrue(orchestrator.shootCyclePID(29.44)).onFalse(shooter.stop()); // TODO: change the RPM
+              driverController.x().whileTrue(orchestrator.shootCyclePID((2000))).onFalse(shooter.stop()); // TODO: change the RPM
               driverController.a().whileTrue(shooter.KsFlywheelCharacterization()).onFalse(shooter.stop());
               driverController.b().onTrue(shooter.runFF());
               driverController.leftTrigger().onTrue(indexer.indexUntilSwitch());
