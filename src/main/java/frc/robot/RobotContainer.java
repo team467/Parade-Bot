70815776654d5ade3c5c02e6bb6cc0026e2bdbe1
Niti <ntyag28@students.shrewsbury.k12.ma.us>
2025-11-12 20:52:24 -0500

    changed setpoint rpm

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -79,8 +79,8 @@ public class RobotContainer {
                       .onFalse(orchestrator.stopAll());
 
               //driverController.x().whileTrue(orchestrator.shootCycleDistance()).onFalse(shooter.stop());
-              driverController.x().whileTrue(orchestrator.shootCyclePID(4500)).onFalse(shooter.stop()); // TODO: change the RPM
-              driverController.a().whileTrue(shooter.KsFlywheelCharacterization());
+              driverController.x().whileTrue(orchestrator.shootCyclePID(2000)).onFalse(shooter.stop()); // TODO: change the RPM
+//              driverController.a().whileTrue(shooter.KsFlywheelCharacterization());
               driverController.leftTrigger().onTrue(indexer.indexUntilSwitch());
 
               driverController.y().onTrue(indexer.indexIntoShooter());
